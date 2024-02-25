33d9239a1112b54e05a5294d68ab3de1b0ecb1a5
Emilio Cliff <emiliocliff@gmail.com>
2024-02-25 15:19:39 +0300

    add railway pq address

--- go-api/start.sh
+++ go-api/start.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "running migrations"
-/app/migrate -path /app/migration -database postgresql://root:secret@postgres:5432/inventorydb?sslmode=disable -verbose up
+/app/migrate -path /app/migration -database postgresql://postgres:D*fd6EDcc5bdA6AG1c4FF6DafFeG33b*@postgres.railway.internal:5432/railway -verbose up
 
 echo "run go app"
 exec "$@"
