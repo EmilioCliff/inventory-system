7b857dbb84ab48b7e4b2052eea4d60a1e3171e14
Emilio Cliff <emiliocliff@gmail.com>
2024-04-01 22:13:22 +0300

    fix start script bug

--- go-api/start.sh
+++ go-api/start.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-. /app/.env
-
 echo "running migrations"
 /app/migrate -path /app/migration -database postgresql://postgres:D*fd6EDcc5bdA6AG1c4FF6DafFeG33b*@postgres.railway.internal:5432/railway -verbose up
 
