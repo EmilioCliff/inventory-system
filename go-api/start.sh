ada9f5b73ff138eeb6fa20952ac76485278084b3
Emilio Cliff <emiliocliff@gmail.com>
2024-04-01 11:06:43 +0300

    new log

--- go-api/start.sh
+++ go-api/start.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "running migrations"
-/app/migrate -path /app/migration -database "$(DB_SOURCE)" -verbose up
+/app/migrate -path /app/migration -database ${{ secrets.DB_SOURCE }} -verbose up
 
 echo "run go app"
 exec "$@"
