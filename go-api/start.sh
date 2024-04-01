1a5a53f475b3bdd42bf3b0f2952ce168148a301a
Emilio Cliff <emiliocliff@gmail.com>
2024-04-01 21:05:40 +0300

    add url

--- go-api/start.sh
+++ go-api/start.sh
@@ -5,7 +5,7 @@ set -e
 . /app/.env
 
 echo "running migrations"
-/app/migrate -path /app/migration -database "$DB_SOURCE"   -verbose up
+/app/migrate -path /app/migration -database postgresql://postgres:D*fd6EDcc5bdA6AG1c4FF6DafFeG33b*@postgres.railway.internal:5432/railway -verbose up
 
 echo "run go app"
 exec "$@"
