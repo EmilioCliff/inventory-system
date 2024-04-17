83d2ed654ea8070b43af3f39f54f5cb2e2708ff7
Emilio Cliff <emiliocliff@gmail.com>
2024-04-17 05:27:41 +0300

    changed docker path

--- go-api/start.sh
+++ go-api/start.sh
@@ -3,6 +3,6 @@
 set -e
 
 echo "running migrations"
-/app/migrate -path /app/migration -database postgresql://postgres:D*fd6EDcc5bdA6AG1c4FF6DafFeG33b*@postgres.railway.internal:5432/railway -verbose up
+/app/migrate -path /app/migration -database postgresql://postgres:D*fd6EDcc5bdA6AG1c4FF6DafFeG33b*@viaduct.proxy.rlwy.net:26578/railway -verbose up
 
 exec "$@"
