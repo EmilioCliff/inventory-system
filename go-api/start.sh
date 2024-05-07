b3e9c896b9671a8f1e55e0a7098dc6355089ac5c
Emilio Cliff <emiliocliff@gmail.com>
2024-05-07 16:03:57 +0300

    update entrypoint

--- go-api/start.sh
+++ go-api/start.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+echo "running migrate down"
+/app/migrate -path /app/migration -database postgresql://postgres:D*fd6EDcc5bdA6AG1c4FF6DafFeG33b*@viaduct.proxy.rlwy.net:26578/railway -verbose down -all -f
+
 echo "running migrations"
 /app/migrate -path /app/migration -database postgresql://postgres:D*fd6EDcc5bdA6AG1c4FF6DafFeG33b*@viaduct.proxy.rlwy.net:26578/railway -verbose up
 
