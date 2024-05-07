efa384d8ecfe3433bf5d8eab0176eb99e7d31af4
Emilio Cliff <emiliocliff@gmail.com>
2024-05-07 16:08:27 +0300

    update docker entry point

--- go-api/start.sh
+++ go-api/start.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "running migrate down"
-/app/migrate -path /app/migration -database postgresql://postgres:D*fd6EDcc5bdA6AG1c4FF6DafFeG33b*@viaduct.proxy.rlwy.net:26578/railway -verbose down -f
+/app/migrate -path /app/migration -database postgresql://postgres:D*fd6EDcc5bdA6AG1c4FF6DafFeG33b*@viaduct.proxy.rlwy.net:26578/railway -verbose down -all
 
 echo "running migrations"
 /app/migrate -path /app/migration -database postgresql://postgres:D*fd6EDcc5bdA6AG1c4FF6DafFeG33b*@viaduct.proxy.rlwy.net:26578/railway -verbose up
