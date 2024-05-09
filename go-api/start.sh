66e37a3f54d6429b1ca769a66d54082a6a4d15de
Emilio Cliff <emiliocliff@gmail.com>
2024-05-09 14:16:37 +0300

    cleared the db

--- go-api/start.sh
+++ go-api/start.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-# echo "running migrate down"
-# /app/migrate -path /app/migration -database postgresql://postgres:D*fd6EDcc5bdA6AG1c4FF6DafFeG33b*@viaduct.proxy.rlwy.net:26578/railway -verbose drop -f
+echo "running migrate down"
+/app/migrate -path /app/migration -database postgresql://postgres:D*fd6EDcc5bdA6AG1c4FF6DafFeG33b*@viaduct.proxy.rlwy.net:26578/railway -verbose drop -f
 
 echo "running migrations"
 /app/migrate -path /app/migration -database postgresql://postgres:D*fd6EDcc5bdA6AG1c4FF6DafFeG33b*@viaduct.proxy.rlwy.net:26578/railway -verbose up
