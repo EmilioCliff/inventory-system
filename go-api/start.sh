7aad91519cf03bcd60600e4994ad14b9586c4818
Emilio Cliff <emiliocliff@gmail.com>
2024-05-18 17:01:05 +0300

    updated the starting script

--- go-api/start.sh
+++ go-api/start.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-echo "running migrate down"
-/app/migrate -path /app/migration -database postgresql://postgres:D*fd6EDcc5bdA6AG1c4FF6DafFeG33b*@viaduct.proxy.rlwy.net:26578/railway -verbose down 2
+# echo "running migrate down"
+# /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose down 2
 
-# echo "running migrations"
-# /app/migrate -path /app/migration -database postgresql://postgres:D*fd6EDcc5bdA6AG1c4FF6DafFeG33b*@viaduct.proxy.rlwy.net:26578/railway -verbose up
+echo "running migrations"
+/app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 
 exec "$@"
